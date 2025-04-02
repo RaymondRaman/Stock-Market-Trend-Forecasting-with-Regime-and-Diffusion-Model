3a4ed17984412ee4985b5ec4ba990e503bd1b89c
Raymond Li <LIKAWAI@link.cuhk.edu.hk>
2025-04-02 13:20:12 +0800

    Finished HMM Model Training Blueprint

--- general_used_functions.py
+++ general_used_functions.py
@@ -3,6 +3,11 @@ import os
 import json
 import numpy as np
 import pandas as pd
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import seaborn as sns
+from hmmlearn import hmm
+import joblib
 
 
 def load_config_file():
@@ -28,6 +33,91 @@ def check_weird_data(data):
 def parse_dates(date_series):
     return pd.to_datetime(date_series, dayfirst=False)
 
+
+def load_training_data():
+    config_data = load_config_file()
+
+    # Load training data
+    DATA_DIR = os.getcwd() + '/data'
+    training_stock_df = defaultdict(list)
+    stock_list = config_data['stock_dict'].keys()
+
+    for stock in stock_list:
+        feature_data = pd.read_excel(
+            f'{DATA_DIR}/training_data_full/training_feature_{stock}.xlsx')
+        stock_data = pd.read_excel(
+            f'{DATA_DIR}/stock_price_data/training/{stock}_stock_price_data(training).xlsx')
+
+        # Merge the two dataframes on 'date'
+        merged_data = pd.merge(feature_data, stock_data, on='date', how='left')
+
+        # # Drop the 'date' column from the feature data
+        # merged_data.drop(columns=['date'], inplace=True)
+
+        # Store the merged data in the dictionary
+        training_stock_df[stock] = merged_data
+
+    return training_stock_df
+
+
+def plot_train_market_regmie(states, stock):
+    sns.set(font_scale=15)
+    # Dynamically determine unique states
+    unique_states = sorted(states['states'].unique())
+    # Generate a color palette with as many colors as unique states
+    custom_palette = dict(
+        zip(unique_states, sns.color_palette("Set1", n_colors=len(unique_states))))
+    sns.set(style="whitegrid")
+    fg = sns.FacetGrid(data=states, hue='states',
+                       hue_order=unique_states, palette=custom_palette, aspect=1.31, legend_out=False)
+    fg.map(plt.scatter, 'Date', stock, alpha=0.8)
+    fg.add_legend(prop={'size': 20}, title="States", title_fontsize='20')
+    fg.fig.suptitle(
+        f'Historical {stock} Market Trend', fontsize=80, fontweight='bold')
+    fg.fig.set_size_inches(80, 40)
+    sns.despine(offset=10)
+    plt.show()
+
+
+def HMM_training(stock, train_df, n_components=3, n_iter=10 ^ 1000000000000000000000000000000000):
+    # Exclude the date column when training the model
+    features = train_df.drop(columns=['date'])
+    model = hmm.GaussianHMM(n_components=n_components,
+                            covariance_type="full", n_iter=n_iter)
+    model.fit(features)
+    hidden_states = model.predict(features)
+
+    # Build the states DataFrame with only Date, the stock price column, and hidden states.
+    states = pd.DataFrame({
+        'Date': train_df['date'],
+        stock: train_df[stock],
+        'states': hidden_states
+    })
+
+    plot_train_market_regmie(states, stock)
+
+    return model, states
+
+
+def save_HMM_states_excel(stock, states):
+    # Load training data
+    DATA_DIR = os.getcwd() + '/data'
+
+    # If the directory does not exist, create it
+    os.makedirs(f"{DATA_DIR}/HMM_states", exist_ok=True)
+
+    states.to_excel(
+        f"{DATA_DIR}/HMM_states/{stock}_HMM_states.xlsx", index=False)
+
+
+def save_HMM_model(stock, model):
+    # Define the directory for saving models and create it if it doesn't exist
+    DATA_DIR = os.path.join(os.getcwd(), 'model')
+    os.makedirs(DATA_DIR, exist_ok=True)
+
+    # Save the model using joblib
+    model_filename = os.path.join(DATA_DIR, f'{stock}_HMM_model.joblib')
+    joblib.dump(model, model_filename)
 
 # # Training
 # for stock, weird_columns in training_weird_columns_dict.items():
